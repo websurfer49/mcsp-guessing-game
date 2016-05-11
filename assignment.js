8527f9ac02b6e6fcf6ecedd74f3e75248d2d5b20
Ryan Sobol <contact@ryansobol.com>
2016-05-11 06:32:42 -0700

    Tweak descriptions

--- assignment.js
+++ assignment.js
@@ -1,84 +1,91 @@
-// Define a function named updateToDoList. It takes one argument:
+// Define a function named updateTodoList that takes one argument.
 //   todoList (<ul> DOM element)
 //
-// The function will iterate over the list items (<li>) in the list and will
-// do the following:
+// The function must iterate over its list items (<li>) and do the following:
 //   * Remove items from the list if its text starts with "COMPLETED"
 //   * Applies the "important" CSS class if its text starts with "URGENT".
-//     Tip: Applying a CSS class means adding on top of what's there.
+//     TIP: Applying a CSS class means adding on top of what's already there.
 //   * Make no change otherwise.
 
 
 
 
-// Define a function named createLinks. It takes one argument:
+// Define a function named createLinks that takes one argument.
 //   links (object)
 //
 // The object has the following structure:
-// {
-//   "<LINK TEXT>": "<LINK URL>",
-//   "<LINK TEXT>": "<LINK URL>",
-//   "<LINK TEXT>": "<LINK URL>",
-//   ...
-// }
-// where <LINK TEXT> will be the label for the link and <LINK URL> will be where
-// url that the link points to.
+//    {
+//      TEXT: "URL",
+//      TEXT: "URL",
+//      TEXT: "URL",
+//      ...
+//    }
 //
-// The function will return an unordered list DOM element of links with the
-// given label and url. For example, Given:
-// {
-//   "Google": "https://www.google.com",
-//   "Facebook": "https://www.facebook.com",
-//   "Yahoo": "https://www.yahoo.com",
-//   "Galvanize": "https://www.galvanize.com"
-// }, it will produce the following:
-// <ul>
-//   <li><a href="https://www.google.com">Google</a></li>
-//   <li><a href="https://www.facebook.com">Facebook</a></li>
-//   <li><a href="https://www.yahoo.com">Yahoo</a></li>
-//   <li><a href="https://www.galvanize.com">Galvanize</a></li>
-// </ul>
+// Where TEXT is the link's text content and URL is the link's href value.
+//
+// The function must return an unordered list DOM element of links with the
+// given label and url. For example, given:
+//    {
+//      "Google": "https://www.google.com",
+//      "Facebook": "https://www.facebook.com",
+//      "GitHub": "https://github.com",
+//      "Galvanize": "https://www.galvanize.com"
+//    }
+//
+// It must return the following:
+//    <ul>
+//      <li><a href="https://www.google.com">Google</a></li>
+//      <li><a href="https://www.facebook.com">Facebook</a></li>
+//      <li><a href="https://github.com">GitHub</a></li>
+//      <li><a href="https://www.galvanize.com">Galvanize</a></li>
+//    </ul>
 
 
 
 
-// Write a function pullQuote which takes in one argument.
+// Write a function named extractQuote that takes in one argument.
 //   article (<article> DOM element)
 //
-// We assume the article will contain a paragraph. Here's an example:
+// Assume the article contains a paragraph. For example:
 //
-// <article>
-//   <p>Neale Donald Walsch once said, "Life begins at the end of your comfort zone." This is really important.</p>
-// </article>
+//    <article>
+//      <p>Neale Donald Walsch once said, "Life begins at the end of your
+//      comfort zone." This is really important.</p>
+//    </article>
 //
-// The function will check the paragraph for double quotes (") and pull it out
-// into a blockquote. It will then replace the paragraph with that blockquote.
-// No changes are made if there are no double quotes. For example, given the
-// above input, it will produce the following:
+// The function must check the paragraph for double quotes ("), extract it out,
+// add it to the text of a <blockquote> element, and then replace the paragraph
+// with that blockquote. For example, given the  above input, it must change the
+// article as following:
 //
-// <article>
-//   <blockquote>"Life begins at the end of your comfort zone."</blockquote>
-// </article>
+//    <article>
+//      <blockquote>"Life begins at the end of your comfort zone."</blockquote>
+//    </article>
 //
-// Tip: Assume that if there is a double quote, there will be a closing double
-// quote in the text.
+// No changes should be made if there's no quote.
+//
+// TIP: Assume that if there's an openning double quote, there's a closing
+// double quote as well.
 
 
 
 
-// Define a function named createTable. It takes one argument:
-//   data (array of arrays) (We can assume that data has at least 3 elements)
+// Define a function named createTable that takes one argument.
+//   data (array of arrays)
 //
-// The function will return a table DOM element that matches the structure of
-// the input data. The first row will be the header, and the last row will be
-// the footer. For example, given the following input:
-// [
-//   ['a', 'b', 'c'],
-//   ['d', 'e', 'f'],
-//   ['g', 'h', 'i'],
-//   ['j', 'k', 'l']
-// ]
-// the function would produce
+// The function must return a <table> DOM element that matches the structure of
+// the input data. The first element in the data array is the <thead> data, the
+// last element is the <tfoot> data, and the remaining elements form the <tbody>
+// data. For example, given the following input:
+//    [
+//      ['a', 'b', 'c'],
+//      ['d', 'e', 'f'],
+//      ['g', 'h', 'i'],
+//      ['j', 'k', 'l']
+//    ]
+//
+// the function returns
+//
 // <table>
 //   <thead>
 //     <tr>
@@ -107,3 +114,6 @@
 //     </tr>
 //   </tfoot>
 // </table>
+//
+// TIP: Assume that data array has at least three elements.
+// TIP: Assume that the elements of the data array are equal in length.
